c382998db12eee60e1088857c68433fef27b32d5
Anthony <anthonymarques@outlook.com>
2020-01-26 18:27:10 +0200

    working on winning losing and draw logic

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
+import { ENGINE_METHOD_PKEY_ASN1_METHS } from "constants";
+
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
-
 let circleTurn;
 
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true });
-});
+startGame();
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
 
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
-
+  if (checkWin(currentClass)) {
+    console.log('winner');
+    endGame(false);
+}
   swapTurns();
-  setBoardHoverClass()
+  setBoardHoverClass();
+}
+
+function endGame(draw) {
+    if(draw) {
+
+    } else {
+        winnerTextMessage
+    } 
 }
 
 function placeMark(cell, currentClass) {
@@ -27,12 +55,20 @@ function swapTurns() {
   circleTurn = !circleTurn;
 }
 
+function checkWin(currentClass) {
+  return WINNING_COMBOS.some(combination => {
+      return combination.every(index => {
+          return cellElements[index].classList.contains(currentClass);
+      })
+  });
+}
+
 function setBoardHoverClass() {
-    board.classList.remove(X_CLASS);
-    board.classList.remove(CIRCLE_CLASS);
-    if(circleTurn) {
-        board.classList.add(CIRCLE_CLASS);
-    } else {
-        board.classList.add(X_CLASS);
-    }
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
 }
